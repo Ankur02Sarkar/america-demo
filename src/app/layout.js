bb5fb2c0f62bb2e8fa6310b4279cc3a37018ca16
Rishi Mungia <rishi.mungia@gmail.com>
2023-11-19 14:33:29 +0000

    updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
-"use client";
 import { Inter } from 'next/font/google';
 
 import './globals.scss'
 
 import Intro from '../Components/Intro/Intro'
 import Header from '../Components/Header/Header'
-import BackgroundVideo from '../Components/BackgroundVideo/BackgroundVideo'
-import { usePathname } from 'next/navigation';
 import Cursor from '../Components/Cursor/Cursor';
 
 const font = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
-  const path = usePathname();
 
   return (
     <html lang="en">
@@ -21,12 +17,12 @@ export default function RootLayout({ children }) {
         <link rel="preload" href="/gun.png" as="image" />
         <link rel="preload" href="/intro.png" as="image" />
         <link rel="preload" href="/intro-smile.png" as="image" />
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </head>
       <body>
         <Header />
         {children}
         <Intro />
-        {path !== '/' ? <BackgroundVideo /> : null}
         <Cursor />
       </body>
     </html>
